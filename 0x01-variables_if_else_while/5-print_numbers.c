5078a8628cc3ef4e1e7425f3eaf94197faa1db18
TheJokermmy <sammykit01@gmail.com>
2023-02-17 17:20:10 -0800

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - assign a random number to the variable n to make it executable
+ * and print the last digit of the number in variable n
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
