75ccef035eb4f12656e626a92a3ca38ea24383b7
TheJokermmy <sammykit01@gmail.com>
2023-02-17 19:35:55 -0800

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,21 @@
-#include <stdio.h>
+/*
+ * File: 101-quote.c
+ * Auth: Brennan D Baraban
+ */
+
+
 #include <unistd.h>
+
 /**
  * main - A c program that prints a line to the standard error
- * Return 1 (Sucess)
+ *
+ * Return: Always 1.
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2,
+			"and that piece of art is useful\" - Dora Korpar, 2015-10-19",
+			59);
+
 	return (1);
 }
